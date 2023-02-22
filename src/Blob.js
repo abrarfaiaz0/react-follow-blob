31e85d8ad7f4f7339d01eb3ef47e46868a3af1da
abrarfaiazatuno <abrarfaiaz0@gmail.com>
2023-02-23 02:00:29 +0600

    finalized

--- src/Blob.js
+++ src/Blob.js
@@ -3,8 +3,28 @@ import "./Blob.css";
 function Blob(props) {
   let X = props.mouse[0];
   let Y = props.mouse[1];
+  let show = false;
+  console.log(document.body.clientWidth - 1, X);
+  if (
+    X - 10 <= 0 ||
+    Y - 10 <= 0 ||
+    X + 10 >= document.body.clientWidth ||
+    Y + 10 >= document.body.clientHeight
+  ) {
+    show = false;
+  } else {
+    show = true;
+  }
 
-  return <div className="blob">ad</div>;
+  return (
+    <>
+      <div
+        className={show ? "blob" : "hidden"}
+        style={{ top: Y, left: X }}
+      ></div>
+      <div className="blur"></div>
+    </>
+  );
 }
 
 export default Blob;
